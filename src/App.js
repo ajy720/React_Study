936f7977f493057bc36d0e816b0253a8431f394e
AnHyeonSeok <ajy720@gmail.com>
2020-04-18 18:19:45 +0900

    life cycle

--- src/App.js
+++ src/App.js
@@ -1,66 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Album({ name, image, rating }) {
-  return (
-    <div>
-      <h3>{name}</h3>
-      <h4>{rating}/5.0</h4>
-      <img src={image} alt={name + " Album cover"} width="200" height="200" />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    // 컴포넌트가 생성 된 직후, 렌더링 전
+    super(props);
+    console.log("hello");
+  }
 
-Album.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
+  state = {
+    count: 0,
+    count2: 0,
+  };
 
-const albums = [
-  {
-    id: 1,
-    rating: 4.5,
-    name: "Boyhood",
-    image:
-      "https://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/321/723/81321723_1575013966692_1_600x600.JPG",
-  },
-  {
-    id: 2,
-    rating: 5,
-    name: "24:26",
-    image:
-      "https://i.pinimg.com/originals/a8/f5/98/a8f5981839d951c4e3eda830152f2ee5.jpg",
-  },
-  {
-    id: 3,
-    rating: 4,
-    name: "The Anecdote",
-    image:
-      "https://w.namu.la/s/f111aea6d5dc7ac53e40449d61f6675e2466ebb6a6b03d33dee50889e30decdf5cd91d3ae02850984fa62314c0277c82ae9a75b6b96a5b471d13ffbf70ec2f85945660cacec9a49f8b7716eb439b89429f0e075875b1fd99f0b32792e3b128d9",
-  },
-  {
-    id: 4,
-    rating: 3.5,
-    name: "OFF DUTY",
-    image:
-      "https://post-phinf.pstatic.net/MjAxOTExMTJfMTY4/MDAxNTczNTQ3ODMyODA5.BOHWv6tnGQ7xYXGLd139nbMpDbdhoC8Ez0fX7mxCuPEg.R2e3scLU5ejSzoeRQG8LBA3tc7Zw9z_R_5jEo38RVbAg.JPEG/OFF_DUTY_커버.jpg?type=w800_q75",
-  },
-];
+  add = () => {
+    this.setState((curret) => ({ count: curret.count + 1 }));
+  };
 
-function renderAlbum(album) {
-  return (
-    <Album
-      key={album.id}
-      name={album.name}
-      image={album.image}
-      rating={album.rating}
-    />
-  );
-}
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    // 컴포넌트가 렌더링 된 직후
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    // 컴포넌트가 업데이트 된 직후
+    console.log("Updated");
+  }
+  componentWillUnmount() {
+    // 컴포넌트가 삭제? 종료? 된 직후
+    console.log("Die");
+  }
 
-function App() {
-  return <div className="App">{albums.map(renderAlbum)}</div>;
+  render() {
+    console.log("rendering");
+    return (
+      <div>
+        <h1>
+          The number is : {this.state.count}, {this.state.count2}
+        </h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
